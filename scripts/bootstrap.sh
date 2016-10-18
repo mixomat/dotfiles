d00909fbaa48a7be3e16a50addb56e3f3c15e1b1
Marc Weinberger <marc.weinberger@me.com>
2016-10-18 22:28:34 +0200

    Initial commit

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# this symlinks all the dotfiles to ~/
+
+# this is safe to run multiple times and will prompt you about anything unclear
+
+# inspired by alrra's nice work:
+#   https://raw.githubusercontent.com/alrra/dotfiles/master/os/create_symbolic_links.sh
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+declare symlinkDirectory="$HOME"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+create_symlinks() {
+
+    declare -a FILES_TO_SYMLINK=(
+
+        "shell/bash_profile"
+        "shell/bashrc"
+        "shell/inputrc"
+
+        "git/git_completion"
+        "git/gitconfig"
+        "git/gitignore"
+#
+#        "tmux/tmux.conf"
+#
+#        "vim/vim"
+#        "vim/vimrc"
+
+    )
+
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+    local skipQuestions=false
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    for i in "${FILES_TO_SYMLINK[@]}"; do
+
+        sourceFile="$(cd .. && pwd)/$i"
+        targetFile="$symlinkDirectory/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ ! -e "$targetFile" ] || $skipQuestions; then
+
+            execute \
+                "ln -fs $sourceFile $targetFile" \
+                "$targetFile → $sourceFile"
+
+        elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+            print_success "$targetFile → $sourceFile"
+        else
+
+
+            ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+            if answer_is_yes; then
+
+                rm -rf "$targetFile"
+
+                execute \
+                    "ln -fs $sourceFile $targetFile" \
+                    "$targetFile → $sourceFile"
+
+            else
+                print_error "$targetFile → $sourceFile"
+            fi
+
+        fi
+
+    done
+
+}
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+    print_in_purple "\n • Create symbolic links\n\n"
+    create_symlinks "$@"
+}
+
+main "$@"
---
+++ scripts/utils.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+answer_is_yes() {
+    [[ "$REPLY" =~ ^[Yy]$ ]] \
+        && return 0 \
+        || return 1
+}
+
+ask() {
+    print_question "$1"
+    read -r
+}
+
+ask_for_confirmation() {
+    print_question "$1 (y/n) "
+    read -r -n 1
+    printf "\n"
+}
+
+execute() {
+
+    local -r CMDS="$1"
+    local -r MSG="${2:-$1}"
+    local -r TMP_FILE="$(mktemp /tmp/XXXXX)"
+
+    local exitCode=0
+    local cmdsPID=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # If the current process is ended,
+    # also end all its subprocesses.
+
+    set_trap "EXIT" "kill_all_subprocesses"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Execute commands in background
+
+    eval "$CMDS" \
+        &> /dev/null \
+        2> "$TMP_FILE" &
+
+    cmdsPID=$!
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Show a spinner if the commands
+    # require more time to complete.
+
+    show_spinner "$cmdsPID" "$CMDS" "$MSG"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait for the commands to no longer be executing
+    # in the background, and then get their exit code.
+
+    wait "$cmdsPID" &> /dev/null
+    exitCode=$?
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Print output based on what happened.
+
+    print_result $exitCode "$MSG"
+
+    if [ $exitCode -ne 0 ]; then
+        print_error_stream < "$TMP_FILE"
+    fi
+
+    rm -rf "$TMP_FILE"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    return $exitCode
+
+}
+
+print_error() {
+    print_in_red "   [✖] $1 $2\n"
+}
+
+print_error_stream() {
+    while read -r line; do
+        print_error "↳ ERROR: $line"
+    done
+}
+
+print_in_green() {
+    printf "\e[0;32m%b\e[0m" "$1"
+}
+
+print_in_purple() {
+    printf "\e[0;35m%b\e[0m" "$1"
+}
+
+print_in_red() {
+    printf "\e[0;31m%b\e[0m" "$1"
+}
+
+print_in_yellow() {
+    printf "\e[0;33m%b\e[0m" "$1"
+}
+
+print_result() {
+
+    if [ "$1" -eq 0 ]; then
+        print_success "$2"
+    else
+        print_error "$2"
+    fi
+
+    return "$1"
+
+}
+
+
+print_question() {
+    print_in_yellow "   [?] $1"
+}
+
+print_success() {
+    print_in_green "   [✔] $1\n"
+}
+
+print_warning() {
+    print_in_yellow "   [!] $1\n"
+}
+
+set_trap() {
+
+    trap -p "$1" | grep "$2" &> /dev/null \
+        || trap '$2' "$1"
+
+}
+
+show_spinner() {
+
+    local -r FRAMES='/-\|'
+
+    # shellcheck disable=SC2034
+    local -r NUMBER_OR_FRAMES=${#FRAMES}
+
+    local -r CMDS="$2"
+    local -r MSG="$3"
+    local -r PID="$1"
+
+    local i=0
+    local frameText=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Display spinner while the commands are being executed.
+
+    while kill -0 "$PID" &>/dev/null; do
+
+        frameText="   [${FRAMES:i++%NUMBER_OR_FRAMES:1}] $MSG"
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Print frame text.
+
+        printf "%s" "$frameText"
+        sleep 0.2
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Clear frame text.
+
+        printf "\r"
+
+    done
+
+}
