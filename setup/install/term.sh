b6c74c347ccf10b8f41a7f14ffe05efa0b20b55e
Marc Weinberger <marc.weinberger@me.com>
2016-10-24 09:36:53 +0200

    fix(term): move term setup files to install dir

--- setup/install/term.sh
+++ setup/install/term.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Install the iterm2 themes
-open "${HOME}/dotfiles/term/themes/Solarized Dark.itermcolors"
-open "${HOME}/dotfiles/term/themes/Molotov.itermcolors"
+open "${HOME}/dotfiles/setup/install/term/Solarized Dark.itermcolors"
+open "${HOME}/dotfiles/setup/install/term/Molotov.itermcolors"
 
-# Don’t display the annoying prompt when quitting iTerm
-defaults write com.googlecode.iterm2 PromptOnQuit -bool false
