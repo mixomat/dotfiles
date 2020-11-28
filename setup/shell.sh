d886018fce8009c09d328ddb5edbb050540a3b52
Marc Weinberger <marc.weinberger@me.com>
2020-11-28 22:38:37 +0100

    feat(setup): add shell setup script

---
+++ setup/shell.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+setup_zsh() {
+  execute \ 
+        "sh -c '$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)'" \
+        "Installing oh-my-zsh"
+}
+
+setup_powerlevel_theme() {
+  execute \ 
+        "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" \
+        "Installing zsh powerlevel10k theme"
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+  print_in_purple "\n • ZSH\n\n"
+
+  setup_zsh
+  setup_powerlevel_theme
+}
+
+main
+# link shell files
+#ln -s ~/dotfiles/shell/zshrc ~/.zshrc
+
+#if [[ "$OSTYPE" == "darwin"* ]]; then
+	#ln -s ~/dotfiles/shell/zshenv.mac ~/.zshenv
+#else
+	#ln -s ~/dotfiles/shell/zshenv ~/.zshenv
+#fi
