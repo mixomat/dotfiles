7e4758cfca3f7a0f43f7f52d8725d768d31fe10b
Marc Weinberger <marc.weinberger@me.com>
2022-10-27 10:12:46 +0200

    chore(exa): cleanup exa functions

--- shell/exa.sh
+++ shell/exa.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env zsh
 
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #        VARIABLES          #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 export EXA_USER="marc.weinberger"
 export EXA_PROJECTS="/Users/marc/projects/exaring"
 export EXA_OPEN="open"
+export PATH="${PATH}:${EXA_PROJECTS}/exaring-env/bin"
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #        ALIASES            #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+alias exap="_exap"
 alias awsume=". awsume"
-alias kdev="awsume dev && kx arn:aws:eks:eu-central-1:873929438979:cluster/bs-k-dev-v1 && export TF_BUCKET=dev.terraform.exaring && export TF_KEY=aws-development"
-alias kprev="awsume prev && kx arn:aws:eks:eu-central-1:157635668512:cluster/bs-k-preview-v1 && export TF_BUCKET=preview.terraform.exaring && export TF_KEY=aws-preview"
-alias kprod="awsume prod && kx arn:aws:eks:eu-central-1:802129380100:cluster/bs-k-prod-v1 && export TF_BUCKET=prod.terraform.exaring && export TF_KEY=aws-production"
+alias exadev="awsume dev"
+alias exaprev="awsume prev"
+alias exaprod="awsume prod"
+alias exacommit="git config user.email 'marc.weinberger@extern.exaring.de'"
+alias kdev="exadev && kx arn:aws:eks:eu-central-1:873929438979:cluster/bs-k-dev-v1 && export TF_BUCKET=dev.terraform.exaring && export TF_KEY=aws-development"
+alias kprev="exaprev && kx arn:aws:eks:eu-central-1:157635668512:cluster/bs-k-preview-v1 && export TF_BUCKET=preview.terraform.exaring && export TF_KEY=aws-preview"
+alias kprod="exaprod && kx arn:aws:eks:eu-central-1:802129380100:cluster/bs-k-prod-v1 && export TF_BUCKET=prod.terraform.exaring && export TF_KEY=aws-production"
 alias unsetAWS="awsume -u"
 alias prodb="_exa_prod_db_tunnel"
 
